c8ec30cf7d3f31a87bd9921323a7aef1a1b1e284
zty <1048972989@qq.com>
2017-09-18 16:48:33 +0800

    JD主要展示左侧导航

--- html/js/JD/JD.js
+++ html/js/JD/JD.js
@@ -40,37 +40,12 @@ $(document).ready(function(){
   });
 
 });
-/*输入框部分未完善*/
-/*$(document).ready(function(){
-	$(".searchinput").focusin(function(){
-		$(".searchinput").removeClass("col-999");
-		$(".searchinput").addClass("col-666");
-		clearInterval(setmaininputvalsfive);
-		$(".searchinput").val("");
-	});
-	$(".searchinput").focusout(function(){
-		$(".searchinput").removeClass("col-666");
-		$(".searchinput").addClass("col-999");
-		if($(".searchinput").val() == ""){
-			var setmaininputvalsone = setmaininputvals();
-		}
-		else{
-			var maininoutvalfocuout = $(".searchinput").val();
-			return  maininoutvalfocuout;
-		}
-	});
-	var setmaininputvalsone = setmaininputvals();
-	var setmaininputvalsfive = setInterval(function(){setmaininputvals()},1000);
-	function setmaininputvals(){
-		var maval = maininputvals();
-		$(".searchinput").val(maval);
-  	}
-
-	function maininputvals(){
-		var inputvals = new Array();
-		var id = Math.ceil(Math.random()*9);
-		inputvals = ['地球仪','电子琴','BB霜','九阳榨汁机','智能马桶','低至七折','派克钢笔','机械键盘','保险箱','88购物街抢百万购物神劵'];
-		var maininputvals = inputvals[id];
-		return maininputvals;
-	};
-});*/
+/*主要显示左侧导航控制部分*/
+$(".main-menu-left-control").mouseenter(function(){
+  $(this).addClass("bg-999395");
+  $(this).find(".main-menu-describe").show();
+});
+$(".main-menu-left-control").mouseleave(function(){
+  $(this).removeClass("bg-999395");
+  $(this).find(".main-menu-describe").hide();
+});
